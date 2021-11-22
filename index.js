291d953c63a340b7d9ff4623e23c86517f322474
Tibor Szasz <tszasz@gmail.com>
2021-11-22 15:55:08 +0100

    refactor(server): wip listener endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +12,22 @@ const WS = new WebSocket.Server({ server });
 
 WS.on('connection', function connection(conn) {
     console.log('connected');
-    const message = JSON.stringify({
-      data: 'OK'
-    });
-    conn.send(message);
+    app.ws = {
+      conn
+    };
+    conn.send(JSON.stringify({
+      data: {
+        event_type: 'connection'
+      }
+    }));
+});
+
+app.post('/listen', (req, res) => {
+  console.log(req.body);
+  if (app.ws) {
+    app.ws.conn.send(req.body);
+  }
+  res.json({});
 });
 
 console.table({
