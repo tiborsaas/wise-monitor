cd76eff9075249a5970b61f664b4f13356b6aa44
Tibor Szasz <tszasz@gmail.com>
2021-12-08 18:43:06 +0100

    refactor(ext): update date structure and create UI

--- chrome_extension/src/bg/background.js
+++ chrome_extension/src/bg/background.js
@@ -6,37 +6,43 @@ function createActionEntry(payload) {
   const eventMap = {
     "transfers#state-change": {
       type: "state_change",
-      key: "current_state",
+      extract: () => data["current_state"],
     },
     "transfers#active-cases": {
       type: "transfer_issue",
-      key: "active_cases",
+      extract: () => data["active_cases"],
     },
     "balances#credit": {
       type: "transfer_received",
-      key: "post_transaction_balance_amount",
+      extract: () => {
+        return {
+          amount: data["amount"],
+          currency: data["currency"],
+        }
+      },
     },
   }
 
   return {
-    type: data[eventMap[event_type]].type,
-    message: data[eventMap[event_type]].key,
+    type: eventMap[event_type].type,
+    message: eventMap[event_type].extract(),
     sent_at,
   }
 }
 
-function initServerConnection(port) {
+function initServerConnection() {
   const url = "ws://127.0.0.1:5000";
   socket = new WebSocket(url);
 
-  socket.onmessage = function (event) {
-    if (event.data) {
-      const { payload } = JSON.parse(event);
-      chrome.browserAction.setBadgeText({text: data });
+  socket.onmessage = function (messageEvent) {
+    if (messageEvent.data) {
+      const payload = JSON.parse(messageEvent.data);
       console.log(payload);
       console.log(actions);
 
-      actions.push(createActionEntry(payload));
+      if (payload.event_type !== 'connection') {
+        actions.push(createActionEntry(payload));
+      }
     }
   };
 
--- chrome_extension/src/browser_action/popup.js
+++ chrome_extension/src/browser_action/popup.js
@@ -1,3 +1,5 @@
+let totalReceived = 0;
+
 chrome.runtime.sendMessage('get-actions', (response) => {
     console.log(response);
     response.forEach(item => {
@@ -16,6 +18,34 @@ function calculateTotalReceived(actions) {
 
 function createItem(msg) {
     const item = document.createElement('li');
-    item.textContent = msg;
+    const time = document.createElement('span');
+
+    let options = { dateStyle: "medium", timeStyle: "medium" };
+    const timeAt = new Date(msg.sent_at).toLocaleString("en-GB", options);
+
+    time.textContent = timeAt;
+
+    if (msg.type === 'transfer_issue') {
+        const badges = createBadges(msg.message);
+        badges.forEach(badge => {
+            item.appendChild(badge);
+        });
+    } else if (msg.type === 'state_change') {
+        item.textContent = msg.message;
+    } else if (msg.type === 'transfer_received') {
+        item.textContent = `${msg.message.amount} ${msg.message.currency}`;
+    }
+
+    item.className = msg.type;
+    item.appendChild(time);
     document.querySelector('#actions').appendChild(item);
+}
+
+function createBadges(issueArr) {
+    return issueArr.map((issue) => {
+        const node = document.createElement('span');
+        node.className = 'badge';
+        node.textContent = issue;
+        return node;
+    });
 }
