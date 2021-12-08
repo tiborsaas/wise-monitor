b207bba1abf8854bae16a92793a74ffe33c023da
Tibor Szasz <tszasz@gmail.com>
2021-12-08 18:46:36 +0100

    feat(ext): add bg.js connect method

--- chrome_extension/src/bg/background.js
+++ chrome_extension/src/bg/background.js
@@ -1,1 +1,24 @@
-console.log('Hello from background.js');
+const actions = [];
+
+function initServerConnection() {
+  const url = "ws://127.0.0.1:5000";
+  socket = new WebSocket(url);
+
+  socket.onmessage = function (messageEvent) {
+    if (messageEvent.data) {
+      const payload = JSON.parse(messageEvent.data);
+      console.log(payload);
+      console.log(actions);
+    }
+  };
+
+  socket.onclose = function (event) {
+    chrome.browserAction.setBadgeText({text: "-" })
+  };
+
+  socket.onerror = function (error) {
+    chrome.browserAction.setBadgeText({text: "err" })
+  };
+}
+
+initServerConnection();
